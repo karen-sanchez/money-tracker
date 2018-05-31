1925baf1b92c46d5c6468fe90454be124914b438
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2018-05-31 15:06:32 -0400

    Added bootstrap collapse dropdown to main dashboard page.

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	function getSum(total, num) {
 	    return total + num;
 	}
-
+	// get item prices, parse, add, and display
 	$('.parent-date').each(function() {		
 		let pricesArray = [];
 		let findPrice = $(this).find('.product-price').text();
@@ -11,7 +11,37 @@ $(document).ready(function() {
 		let parseArray = pricesArray.join(' ').split(' ').map(parseFloat);
 		let filterArray = parseArray.filter(Boolean);
 		let total = filterArray.reduce(getSum);
-		$(this).find('.categories-total').text('GRAND TOTAL: $ ' + ' ' + total);
+		$(this).find('.categories-total').text('Total: $ ' + ' ' + total);
+	});
+	// create unique attr names for collapse table to work
+	$('.card-header').each(function(i) {
+		$(this).attr('id', 'heading' + i)
+	});
+
+	$('.collapse').each(function(i) {
+		$(this).attr('aria-labelledby', 'heading' + i)
+	});
+
+	$('.btn-link').each(function(i) {
+		$(this).attr('data-target', '#collapse' + i)
+	});
+
+	$('.btn-link').each(function(i) {
+		$(this).attr('aria-controls', 'collapse' + i)
+	});
+
+	$('.collapse').each(function(i) {
+		$(this).attr('id', 'collapse' + i)
+	});
+
+	$('#accordion').on('show hide', function() {
+	    $(this).css('height', 'auto');
+	});
+	// fixes accordion not closing last card
+	$('[id^=collapse]').each(function() {
+		$(this).parent().find('.btn-link').click(function() {
+	  		$(this).parent().parent().next().toggleClass('show');
+		});
 	});
 
 });

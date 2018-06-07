6291c156a8a66d8d704ac59ba8d737d66b97c433
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2018-06-07 17:54:47 -0400

    Organized CSS file. Sidenav is responsive.

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -2,18 +2,18 @@ var Navbar = React.createClass({
 
 	render() {
 		return (
-			<aside className="col-12 col-md-2 p-0 bg-dark">
-				<nav className="navbar navbar-expand navbar-dark bg-dark flex-md-column flex-row">
+			<aside className="col-12 col-md-2 p-0 sidenav-parent">
+				<nav className="navbar navbar-expand sidenav flex-md-column flex-row">
 					<div className="collapse navbar-collapse">
 						<ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
 							<li className="nav-item">
-								<a className="nav-link" href="#"><i className="fab fa-3x fa-medium"></i></a>
+								<a href="/"><img src="/assets/tracker-logo.png" className="nav-logo" /></a>
 							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="/"><i className="fas fa-3x fa-home"></i></a>
+							<li className="nav-item my-auto">
+								<a className="nav-link" href="/"><i className="fas fa-2x fa-home"></i></a>
 							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="/categories"><i className="fas fa-3x fa-plus-circle"></i></a>
+							<li className="nav-item my-auto">
+								<a className="nav-link" href="/categories"><i className="fas fa-2x fa-plus-circle"></i></a>
 							</li>
 						</ul>
 					</div>

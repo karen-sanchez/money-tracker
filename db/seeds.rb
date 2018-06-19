2574b24b58225f2c266f908152a364a165c0143f
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2018-06-19 18:36:08 -0400

    reset db for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.create(name: 'Unicorn Party', user_id: 2, due_date: 2.days.ago)
-Category.create(name: 'Coachella Event', user_id: 1, due_date: 40.days.ago)
-Category.create(name: 'Chris Bday', user_id: 1, due_date: 1.days.ago)
-Category.create(name: 'Dizzys Coca Cola', user_id: 1, due_date: 70.days.ago)

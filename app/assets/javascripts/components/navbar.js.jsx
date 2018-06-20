5cc14b32ed5b14f53d8c4ab452545408f75a0acb
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2018-06-20 19:01:11 -0400

    Image path

--- app/assets/javascripts/components/navbar.js.jsx
+++ app/assets/javascripts/components/navbar.js.jsx
@@ -7,7 +7,7 @@ var Navbar = React.createClass({
 					<div className="collapse navbar-collapse">
 						<ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
 							<li className="nav-item">
-								<a href="/"><img src="assets/tracker-logo.png" className="nav-logo" /></a>
+								<a href="/"><img src="https://expensestrackerapp.herokuapp.com/assets/tracker-logo-f9275070a7535bbc9f92f2026b0493d85cb51aed43c50366585c8ae3717006a3.png" className="nav-logo" /></a>
 							</li>
 							<li className="nav-item my-auto">
 								<a className="nav-link" href="/"><i className="fas fa-2x fa-home"></i></a>
